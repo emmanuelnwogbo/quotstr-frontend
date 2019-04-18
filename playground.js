dab23c8d47d0b6b6b7702ff965b0b110785823f7
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-04-18 06:06:43 +0100

    react-setup

---
+++ playground.js
@@ -0,0 +1,5 @@
+axios.get("https://polar-shelf-78995.herokuapp.com/").then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err)
+})
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>say hello</p>
+    </div>
+  )
+}
+
+export default App;
